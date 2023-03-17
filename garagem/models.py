b1da1de18f53421c6609ec4d1872bc123ca1f235
GiovanniRezende@gmail.com <analuizadiasdarocha212@gmail.com>
2023-03-17 15:08:25 -0300

    Criação dos modelos: Acessório, Cor e Veículo

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,27 @@ class Categoria(models.Model):
 
     def __str__(self):
         return f"{self.descricao} ({self.id})"
+
+class Acessório(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
+    
+class Veículo(models.Model):
+    descricao = models.CharField(max_length=100)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(null=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
+    modelo = models.CharField(max_length=100)
+
+    def __str__(self):
+            return f"{self.marca} {self.modelo} ({self.ano}), cor: {self.cor}"
