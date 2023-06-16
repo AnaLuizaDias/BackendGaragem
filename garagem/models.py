8ea80e78bf098431ea061865ecbc1b6911897345
Ana <analuizadiasdarocha212@gmail.com>
2023-06-16 14:07:58 -0300

    Relação n pra n: Veículo-Acessório

--- garagem/models.py
+++ garagem/models.py
@@ -44,6 +44,7 @@ class Veiculo(models.Model):
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
+    acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")
 
     def __str__(self):
             return f"{self.marca} {self.modelo} ({self.ano}), {self.cor}"
