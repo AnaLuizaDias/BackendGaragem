833936e570ce157d8c0a0bde349563fc1c69ccee
Ana <analuizadiasdarocha212@gmail.com>
2023-06-25 16:26:25 -0300

    Django Model Permissions

--- garagem/views.py
+++ garagem/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+# from rest_framework.permissions import IsAuthenticated
 
 from garagem.models import Marca,Categoria,Cor, Acessorio, Modelo, Veiculo
 from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer, AcessorioSerializer, ModeloSerializer, VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
@@ -10,6 +11,7 @@ class MarcaViewSet(ModelViewSet):
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    # permission_classes = [IsAuthenticated]
 
 class CorViewSet(ModelViewSet):
     queryset = Cor.objects.all()
